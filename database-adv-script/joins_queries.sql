f0a1270b71eef45df678adaf3aa23dbb4ec79374
kholoudxs55kh <kfattem@gmail.com>
2024-12-01 03:21:58 +0200

    fix

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -22,7 +22,8 @@ SELECT
     r.rating,
     r.comment
 FROM Property p
-LEFT JOIN Review r ON p.property_id = r.property_id;
+LEFT JOIN Review r ON p.property_id = r.property_id
+ORDER BY p.name, r.rating DESC;
 
 -- Full Outer
 SELECT
