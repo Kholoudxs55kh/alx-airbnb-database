2354bad8f6112c339b6a153b9baf4c6030af1be0
kholoudxs55kh <kfattem@gmail.com>
2024-12-02 16:28:21 +0200

    db: Refactor complex queries to improve performance.

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,15 @@
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.status AS booking_status,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.name AS property_name,
+    pay.amount,
+    pay.payment_date
+FROM Booking b
+INNER JOIN "User" u ON b.user_id = u.user_id
+INNER JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
