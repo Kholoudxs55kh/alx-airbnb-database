68f7351a72bcd4b0395052c175e2e0a081ff497c
kholoudxs55kh <kfattem@gmail.com>
2024-12-02 16:31:16 +0200

    db: Refactor complex queries to improve performance.

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,3 +1,4 @@
+EXPLAIN ANALYZE
 SELECT
     b.booking_id,
     b.start_date,
@@ -7,9 +8,13 @@ SELECT
     u.last_name,
     u.email,
     p.name AS property_name,
+    p.location,
+    pay.payment_id,
     pay.amount,
     pay.payment_date
 FROM Booking b
 INNER JOIN "User" u ON b.user_id = u.user_id
 INNER JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND pay.payment_method = 'credit_card';
